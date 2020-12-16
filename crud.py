8df31fa42fa62fc042ae48e798a44e3a4c1954e2
li-lauren <laurenlidev@gmail.com>
2020-12-16 21:03:36 +0000

    Add methods for GET and updating tokens

--- crud.py
+++ crud.py
@@ -33,19 +33,50 @@ def getToken(code):
 
 def refreshToken(refresh_token):
     token_url = 'https://accounts.spotify.com/api/token'
-    authorization = 'Basic ' + base64.standard_b64encode(
-        SPOTIFY_CLIENT_ID + ':' + SPOTIFY_SECRET)
+    authorization = 'Basic ' + base64.standard_b64encode(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_SECRET)
 
     headers = {'Authorization': authorization, 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'}
-	body = {'refresh_token': refresh_token, 'grant_type': 'refresh_token'}
-	post_response = requests.post(token_url, headers=headers, data=body)
+    body = {'refresh_token': refresh_token, 'grant_type': 'refresh_token'}
+    post_response = requests.post(token_url, headers=headers, data=body)
 
 	# 200 code indicates access token was properly granted
-	if post_response.status_code == 200:
-		return post_response.json()['access_token'], post_response.json()['expires_in']
-	else:
-		logging.error('refreshToken:' + str(post_response.status_code))
-		return None
+    if post_response.status_code == 200:
+        return post_response.json()['access_token'], post_response.json()['expires_in']
+    else:
+        logging.error('refreshToken:' + str(post_response.status_code))
+        return None
+
+
+def checkTokenStatus(session):
+    
+    if time.time() > session['token_expiration']:
+        # token has expired, refresh token
+        payload = refreshToken(session['refresh_token'])
+
+        if payload:
+            session['token'] = payload[0]
+            session['token_expiration'] = time.time() + payload[1]
+        
+        else: 
+            logging.error('checkTokenStatus')
+            return None
+
+    return "Success"
+
+
+def getReq(session, url, params={}):
+    headers = {'Authorization': f'Bearer {session['token']}'}
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+
+    elif response.status_code == 401 and checkTokenStatus(sesion) != None:
+        return getReq(session, url, params)
+
+    else:
+        logging.error('getReq:' + str(response.status_code))
+        return None
 
 
 if __name__ == '__main__':
