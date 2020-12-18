35525c36d6250e1e831e98c6505c43a79b7c49cb
li-lauren <laurenlidev@gmail.com>
2020-12-18 00:35:09 +0000

    Add playlists state

--- static/js/PlaylistLib.jsx
+++ static/js/PlaylistLib.jsx
@@ -1,8 +1,15 @@
 const PlaylistLib = () => {
-
+    const [playlists, setPlaylists] = useState(null)
+    
     const getPlaylists = () => {
         fetch('/playlists')
         .then(res => res.json())
         .then(data => console.log(data))
     }
+
+    return(
+        <div>
+            Playlists
+        </div>
+    )
 }
