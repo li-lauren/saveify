64e170e9a1dde1a7eddab1148b61dcabf305f727
li-lauren <laurenlidev@gmail.com>
2020-12-29 22:45:24 +0000

    Send playlist id to backend when requesting playlist tracks

--- static/js/Playlist.jsx
+++ static/js/Playlist.jsx
@@ -1,11 +1,18 @@
 const Playlist = ({playlist}) => {
+    const [tracks, setTracks] = useState([]);
 
     const getTracks = () => {
-        fetch('/tracks')
+        fetch(`/tracks/${playlist.id}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setTracks(data)
+        })
     }
     return(
         <div>
             <span onClick={getTracks}>{playlist.name}</span>  
+            {tracks.map(track => <span>{}</span>)}
         </div>
     )
 }
