0641fc327933acb94db667c69990f7451a49d1f8
li-lauren <laurenlidev@gmail.com>
2021-02-18 17:38:07 +0000

    Add model for saved playlists

--- model.py
+++ model.py
@@ -1,8 +1,27 @@
 """Models for Spotify App."""
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from dataclasses import dataclass
 
 db = SQLAlchemy()
+
+@dataclass
+class SavedPlaylist(db.Model):
+    user_id: str
+    orig_playlist_id: str
+    saved_playlist_id: str
+    interval: str
+
+    __tablename__ = "savedPlaylists"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    orig_playlist_id = db.Column(db.String, nullable=False)
+    saved_playlist_id = db.Column(db.String, nullable=False)
+    interval = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<SavedPlaylist id={self.saved_playlist_id} user_id={self.user_id}>'
+
 
 def connect_to_db(flask_app, db_uri='postgresql:///spotify_db', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
