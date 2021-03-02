2bcaef9b436cf6dbc5b88b95d79249bd27d89265
li-lauren <laurenlidev@gmail.com>
2021-03-02 23:55:34 +0000

    Incorporate VCR

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import unittest
 from server import app
+import vcr
+
+from urllib.parse import urlparse
+import requests
+import crud
 
 class FlaskTests(unittest.TestCase):
 
@@ -14,12 +19,26 @@ class FlaskTests(unittest.TestCase):
         """Work to be done after each test."""
         print('Tested')
 
-    def test_home_page(self):
+    def test_home(self):
         """Test for initial home page route."""
 
         result = self.client.get("/")
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Saveify', result.data)
+
+    def test_auth(self):
+        """Test redirection to Spotify OAuth."""
+
+        # expectedPath = 'https://accounts.spotify.com/authorize?'
+        result = self.client.get("/authorize")
+        self.assertEqual(result.status_code, 302)
+        # self.assertIn(expectedPath, urlparse(result.location).path)
+
+    @vcr.use_cassette('fixtures/vcr_cassettes/google_test.yaml')
+    def test_google(self):
+        response = requests.get('https://google.com')
+        self.assertEqual(response.status_code, 200)
+    
 
 
 if __name__ == '__main__':
