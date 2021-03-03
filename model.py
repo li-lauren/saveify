7b91c32dc6b2e2c2160b7e699eb59fe628ce9354
li-lauren <laurenlidev@gmail.com>
2021-03-03 22:04:38 +0000

    Integrate DB testing

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
+import os
 
 db = SQLAlchemy()
 
@@ -45,6 +46,35 @@ class SavedPlaylist(db.Model):
 
     def __repr__(self):
         return f'<SavedPlaylist title={self.title} user_id={self.user_id}>'
+
+
+def example_data():
+    """Create sample data for testing."""
+
+    # In case this query runs more than once, remove existing data
+    SavedPlaylist.query.delete()
+    User.query.delete()
+
+    # Populate with sample user and playlists
+    user1 = User(
+        user_id = 1,
+        access_token = os.environ['ACCESS_TOKEN'],
+        refresh_token = os.environ['REFRESH_TOKEN'],
+        expiration = os.environ['EXPIRATION'],
+        spotify_id = os.environ['SPOTIFY_USER']
+    )
+
+    # savedPlaylist = SavedPlaylist(
+    #     user_id = 1, 
+    #     orig_playlist_id = '37i9dQZEVXcHh9eQliKRvV',
+    #     saved_playlist_id = '2PBhJSpglqQCo5ND349wNs',
+    #     interval = 'weekly', 
+    #     title='VCR DB Test Playlist'
+    # )
+
+    db.session.add(user1)
+    db.session.commit()
+
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///spotify_db', echo=False):
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import unittest
+import json
 from server import app
+from model import connect_to_db, example_data, db
 import vcr
 
 from urllib.parse import urlparse
@@ -20,9 +22,17 @@ class FlaskTests(unittest.TestCase):
         self.client = app.test_client()
         app.config['TESTING'] = True
         app.config['SECRET_KEY'] = 'testkey'
+
+        #Connect to test database
+        connect_to_db(app, "postgresql:///testdb")
+
+        #Create tables and add sample data
+        db.create_all()
+        example_data()
         
         with self.client as c:
             with c.session_transaction() as sess:
+                sess['user_id'] = 1
                 sess['token'] = ACCESS_TOKEN
                 sess['refresh_token'] = REFRESH_TOKEN
                 sess['token_expiration'] = EXPIRATION
@@ -31,12 +41,12 @@ class FlaskTests(unittest.TestCase):
         """Work to be done after each test."""
         print('Tested')
 
-    # def test_home(self):
-    #     """Test for initial home page route."""
+    def test_home(self):
+        """Test for initial home page route."""
 
-    #     result = self.client.get("/")
-    #     self.assertEqual(result.status_code, 200)
-    #     self.assertIn(b'Saveify', result.data)
+        result = self.client.get("/")
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'Saveify', result.data)
 
     def test_auth(self):
         """Test redirection to Spotify OAuth."""
@@ -48,16 +58,37 @@ class FlaskTests(unittest.TestCase):
 
     @vcr.use_cassette('fixtures/vcr_cassettes/google_test.yaml')
     def test_google(self):
+        """Sample test for vcr.py."""
+
         response = requests.get('https://google.com')
         self.assertEqual(response.status_code, 200)
 
-    # @vcr.use_cassette('fixtures/vcr_cassettes/get_playlists_test.yaml')
+    @vcr.use_cassette('fixtures/vcr_cassettes/get_playlists_test.yaml')
     def test_get_playlists(self):
+        """Test getting a list of a user's playlists from Spotify API."""
+
         result = self.client.get("/playlists")
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Discover Weekly', result.data)
         # response = crud.getPlaylists(self.client.session_transaction())
         # self.assertEqual(response.status_code, 200)
+
+    @vcr.use_cassette('fixtures/vcr_cassettes/save_playlist_test.yaml')
+    def test_save_playlist(self):
+        """Test saving a playlist and copying over initial tracks."""
+
+        body = {
+            'title':'VCR Test Playlist', 
+            'interval':'weekly',
+            'playlist_id':'37i9dQZEVXcHh9eQliKRvV'
+        }
+        result = self.client.post("/save", 
+        json=body,
+        follow_redirects=True
+        )
+
+        self.assertIn(b'snapshot_id', result.data)
+
 
 
 if __name__ == '__main__':
