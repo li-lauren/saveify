3e3a555301ddb2ba336d44dd0525d30ac31125f2
li-lauren <laurenlidev@gmail.com>
2021-03-14 21:31:50 +0000

    Add PlaylistScrollBox component

---
+++ static/js/PlaylistScrollBox.jsx
@@ -0,0 +1,44 @@
+const PlaylistScrollBox = ({playlists}) => {
+    const [tracks, setTracks] = useState([]);
+    const [selectedPL, setSelectedPL] = useState(null);
+
+    const getTracks = () => {
+        fetch(`/tracks/${selectedPL}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.items);
+            setTracks(data.items);
+        });
+    }; 
+
+    useEffect(() => {
+        if (selectedPL) {
+            getTracks()
+        } else {
+            setSelectedPL(playlists[0].id)
+        }
+    }, [selectedPL, playlists])
+
+    return(
+        <div className="saved-section">
+            <div id="tracks-container">
+                {
+                    tracks.map((track, i) => <p key={i}>{track.track.name}</p>) 
+                }
+            </div>
+            <div className="scroll-box">
+           
+                {playlists.map((playlist, i) => 
+                    <SavedPlaylist 
+                        key={i} 
+                        playlist={playlist}
+                        setSelectedPL={setSelectedPL}
+                    />
+                )}
+                
+            </div>
+
+        </div>
+        
+    )
+}
